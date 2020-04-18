d9ba379fe50687322a633c6352397ec5793e5daa
damianra <44167699+damianra@users.noreply.github.com>
2020-04-18 22:03:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask
 from flask import render_template
 from flask_restful import Api
-from view import AllData
+from view import AllData, Date, DateRange
 
 # Create flask application and API rest
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SECRET_KEY'
+app.config['SECRET_KEY'] = 'asdaskmdoqwdifnxcmdmqwopomfeo13403'
 app.config['JSON_SORT_KEYS'] = False
 api = Api(app, prefix="/api/v1")
 
 
 api.add_resource(AllData, '/alldata')
-
+api.add_resource(Date, '/date')
+api.add_resource(DateRange, '/range')
 
 if __name__ == '__main__':
 
