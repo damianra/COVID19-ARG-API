52e173497e6c0ba5dfb0c54f9ea76a9f366f9cb3
damianra <44167699+damianra@users.noreply.github.com>
2020-04-18 21:11:15 +0000

    Update view.py

--- view.py
+++ view.py
@@ -1,7 +1,9 @@
 from flask import jsonify
 
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 # Models
+from sqlalchemy import and_
+
 from models import Session, Argentina
 
 
@@ -14,5 +16,41 @@ class AllData(Resource):
         session.close()
         # Return all data in JSON
         return jsonify({
-            'data': [result.serialized for result in argData]
+            'data': [result.serialized for result in argData],
+            'disclaimer': 'Todos los datos fueron recolectados de los reportes diarios del ministerio de salud de Argentina https://www.argentina.gob.ar/coronavirus/informe-diario'
+        })
+
+
+class Date(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('date', help='This field cannot be blank', required=True)
+        session = Session()
+        date_ = parser.parse_args()
+        arg = session.query(Argentina).filter(Argentina.date == date_['date'])
+        session.close()
+        return jsonify({
+            'data': [result.serialized for result in arg],
+            'disclaimer': 'Todos los datos fueron recolectados de los reportes diarios del ministerio de salud de Argentina https://www.argentina.gob.ar/coronavirus/informe-diario'
+        })
+
+
+class DateRange(Resource):
+    def get(self):
+        # Object parser and create argument
+        parser = reqparse.RequestParser()
+        parser.add_argument('startdate', help='This field cannot be blank', required=True)
+        parser.add_argument('enddate', help='This field cannot be blank', required=True)
+        session = Session()
+        data = parser.parse_args()
+        startdate = data['startdate']
+        enddate = data['enddate']
+        arg = session.query(Argentina).filter(and_(
+            Argentina.date >= startdate,
+            Argentina.date <= enddate
+        )).all()
+        session.close()
+        return jsonify({
+            'data': [result.serialized for result in arg],
+            'disclaimer': 'Todos los datos fueron recolectados de los reportes diarios del ministerio de salud de Argentina https://www.argentina.gob.ar/coronavirus/informe-diario'
         })
